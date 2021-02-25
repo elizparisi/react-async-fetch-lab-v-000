7f90c9aa0d0ee032381e1dc2241890f120797b05
Liz P <parisielizabeth@gmail.com>
2021-02-25 02:43:02 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ import React, { Component } from 'react';
 class App extends Component {
   
   componentDidMount() {
-    fetch('')
+    fetch('http://api.open-notify.org/astros.json,')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({peopleInSpace: data.people})
+      })
   }
 }
 
 export default App
-
-componentDidMount() {
-    fetch('http://api.open-notify.org/astros.json')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          peopleInSpace: data.people
-        })
-      })
-  }
-}
