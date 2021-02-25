1ceed8e34c0bfd15f04aaac2fdcdf23e688c6e3a
Liz P <parisielizabeth@gmail.com>
2021-02-25 02:41:19 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,5 +3,20 @@ import React, { Component } from 'react';
 
 class App extends Component {
   
-  componentDidMount()
+  componentDidMount() {
+    fetch('')
+  }
+}
+
+export default App
+
+componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  }
 }
